7cfc70d32783acd0a7f7b2fdf18e398b29b46494
MondriaanBI <businessinformation@rocmondriaan.nl>
2025-02-05 16:26:55 +0100

    Add files via upload
    Uploaded updated .py files and temporary .ipynb files (work in progress)

--- Uitnodigingsregel/Module/modeling/predict.py
+++ Uitnodigingsregel/Module/modeling/predict.py
@@ -1,30 +1,25 @@
+import pandas as pd
 from pathlib import Path
+import joblib
+import os
+from module.config import *
 
-import typer
-from loguru import logger
-from tqdm import tqdm
+best_rf_model = joblib.load('models/random_forest_regressor.joblib')
 
-from Module.config import MODELS_DIR, PROCESSED_DATA_DIR
-
-app = typer.Typer()
-
-
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "test_features.csv",
-    model_path: Path = MODELS_DIR / "model.pkl",
-    predictions_path: Path = PROCESSED_DATA_DIR / "test_predictions.csv",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Performing inference for model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Inference complete.")
-    # -----------------------------------------
-
-
-if __name__ == "__main__":
-    app()
+# Load Random Forest Regressor model for predictions
+def randomforestregressormodel_pred (pred_df, random_state = RANDOM_SEED):
+    X_pred = pred_df.values 
+    X_pred_studentnumber = pred_df[['Studentnummer']]
+    # Add columns for specific Power-BI output
+    yhat2 = best_rf_model.predict(X_pred)
+    pred_data0 = pd.DataFrame({"yhat2": yhat2})
+    pred_data = pd.concat([pred_data0, X_pred_studentnumber],axis=1).reindex(pred_data0.index)
+    pred_data.rename(columns={0: 'StudentNr'}, inplace=True)
+    
+    # Sort results
+    pred_data['ranking'] = pred_data['yhat2'].rank(method = 'dense', ascending=False)
+    pred_data = pred_data.sort_values(by=['yhat2'], ascending=False).reset_index(drop=True)
+    student_ranked_data = pd.DataFrame(data = pred_data)
+    student_ranked_data = student_ranked_data.rename({'yhat2': 'voorspelling'}, axis=1)
+    student_ranked_data_ordered = student_ranked_data[['ranking', 'Studentnummer', 'voorspelling']]
+    return student_ranked_data_ordered
--- Uitnodigingsregel/Module/modeling/train.py
+++ Uitnodigingsregel/Module/modeling/train.py
@@ -1,30 +1,27 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
 from pathlib import Path
+from module.config import *
 
-import typer
-from loguru import logger
-from tqdm import tqdm
+# Random Forest Regressor model for training 
+def randomforestregressormodel_train(dataset_train):
+    X = dataset_train.drop("Dropout", axis=1).values
+    y = dataset_train.Dropout.values
 
-from Module.config import MODELS_DIR, PROCESSED_DATA_DIR
+    rf = RandomForestRegressor(random_state=RANDOM_SEED)
+    rf_parameters = {
+    'bootstrap': [True, False],
+    'max_depth': [2, 3, 4],
+    'max_features': [3, 4, 5],
+    'min_samples_leaf': [3, 4, 5],
+    'min_samples_split': [2, 3, 5],
+    'n_estimators': [100, 200, 300]}
 
-app = typer.Typer()
-
-
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "features.csv",
-    labels_path: Path = PROCESSED_DATA_DIR / "labels.csv",
-    model_path: Path = MODELS_DIR / "model.pkl",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Training some model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Modeling training complete.")
-    # -----------------------------------------
+    grid_model = GridSearchCV(rf, rf_parameters, n_jobs = -1, verbose = 3)
+    grid_model.fit(X, y) 
+    best_params = grid_model.best_params_
+    best_rf_model = RandomForestRegressor(**best_params)
+    best_rf_model.fit(X, y) 
+    return best_rf_model
 
 
-if __name__ == "__main__":
-    app()
