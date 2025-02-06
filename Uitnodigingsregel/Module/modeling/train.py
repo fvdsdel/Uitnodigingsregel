74706b5da8eea804eea992fa87e53e17b34eff6c
MondriaanBI <businessinformation@rocmondriaan.nl>
2025-02-06 14:05:57 +0100

    Add files via upload
    Updated train.py file

--- Uitnodigingsregel/Module/modeling/train.py
+++ Uitnodigingsregel/Module/modeling/train.py
@@ -7,15 +7,8 @@ from module.config import *
 def randomforestregressormodel_train(dataset_train):
     X = dataset_train.drop("Dropout", axis=1).values
     y = dataset_train.Dropout.values
-
+    
     rf = RandomForestRegressor(random_state=RANDOM_SEED)
-    rf_parameters = {
-    'bootstrap': [True, False],
-    'max_depth': [2, 3, 4],
-    'max_features': [3, 4, 5],
-    'min_samples_leaf': [3, 4, 5],
-    'min_samples_split': [2, 3, 5],
-    'n_estimators': [100, 200, 300]}
 
     grid_model = GridSearchCV(rf, rf_parameters, n_jobs = -1, verbose = 3)
     grid_model.fit(X, y) 
