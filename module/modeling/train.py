8dcda130cbd949b625600c129d2bc7f2bb70bb21
MondriaanBI <businessinformation@rocmondriaan.nl>
2025-02-11 15:45:37 +0100

    Add files via upload
    Updated predict and train files

--- module/modeling/train.py
+++ module/modeling/train.py
@@ -1,20 +1,50 @@
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import Lasso
+from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
-from pathlib import Path
+import joblib
 from module.config import *
 
 # Random Forest Regressor model for training 
 def randomforestregressormodel_train(dataset_train):
     X = dataset_train.drop("Dropout", axis=1).values
     y = dataset_train.Dropout.values
-    
-    rf = RandomForestRegressor(random_state=RANDOM_SEED)
+    rf = RandomForestRegressor(random_state=random_seed)
 
-    grid_model = GridSearchCV(rf, rf_parameters, n_jobs = -1, verbose = 3)
+    grid_model = GridSearchCV(rf, rf_parameters, n_jobs = -1, verbose = 2)
     grid_model.fit(X, y) 
     best_params = grid_model.best_params_
     best_rf_model = RandomForestRegressor(**best_params)
     best_rf_model.fit(X, y) 
+    
+    joblib.dump(best_rf_model, 'models/random_forest_regressor.joblib')
     return best_rf_model
 
+def lassoregressionmodel_train (dataset_train_sdd): 
+    X = dataset_train_sdd.drop("Dropout", axis=1)
+    y = dataset_train_sdd.Dropout.values
+    lasso_model = Lasso(random_state = random_seed)
+    param = {'alpha':alpha_range}
 
+    lasso_grid_search = GridSearchCV(lasso_model, param_grid = param, cv=5, n_jobs = -1, verbose = 2)
+    lasso_grid_search.fit(X, y)
+
+    best_params = lasso_grid_search.best_params_
+    best_lasso_model = Lasso(**best_params)
+    best_lasso_model.fit(X, y) 
+    
+    joblib.dump(best_lasso_model, 'models/lasso_regression.joblib')
+    return best_lasso_model
+
+def supportvectormachinemodel_train(dataset_train):
+    X = dataset_train.drop("Dropout", axis=1).values
+    y = dataset_train.Dropout.values
+    
+    svm_gridsearch = GridSearchCV(SVC(random_state=random_seed, probability = True), svm_parameters, refit = True, n_jobs = -1, verbose = 2) 
+    svm_gridsearch.fit(X, y)
+
+    best_params = svm_gridsearch.best_params_
+    best_svm_model = SVC(**best_params, probability = True)
+    best_svm_model.fit(X, y) 
+    joblib.dump(best_svm_model, 'models/support_vector_machine.joblib')
+    return best_svm_model
