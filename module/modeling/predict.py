8797bfeee7a92e298ce3bdbf34aeddd30c0d824c
MondriaanBI <businessinformation@rocmondriaan.nl>
2025-03-04 10:30:15 +0100

    Updated
    The studentnumber_column is now included in the train and predict models, so users can fill in the column name of student number of their own dataset in the config file.

--- module/modeling/predict.py
+++ module/modeling/predict.py
@@ -8,8 +8,8 @@ best_svm_model = joblib.load('models/support_vector_machine.joblib')
 
 # Load Random Forest Regressor model for predictions
 def randomforestregressormodel_pred (pred_df, random_state = random_seed):
-    X_pred = pred_df.drop("Dropout", axis=1).values 
-    X_pred_studentnumber = pred_df[['Studentnummer']]
+    X_pred = pred_df.drop(dropout_column, axis=1).values 
+    X_pred_studentnumber = pred_df[[studentnumber_column]]
     
     yhat2 = best_rf_model.predict(X_pred)
     pred_data0 = pd.DataFrame({'voorspelling': yhat2})
@@ -20,13 +20,13 @@ def randomforestregressormodel_pred (pred_df, random_state = random_seed):
     pred_data['ranking'] = pred_data['voorspelling'].rank(method = 'dense', ascending=False)
     pred_data = pred_data.sort_values(by=['voorspelling'], ascending=False).reset_index(drop=True)
     student_ranked_data = pd.DataFrame(data = pred_data)
-    student_ranked_data_ordered = student_ranked_data[['ranking', 'Studentnummer', 'voorspelling']]
+    student_ranked_data_ordered = student_ranked_data[['ranking', studentnumber_column, 'voorspelling']]
     return student_ranked_data_ordered
 
 # Lasso regression model for predicitng
 def lassoregressionmodel_pred (pred_df_sdd, dataset_pred, random_state = random_seed):
-    X_pred = pred_df_sdd.drop("Dropout", axis=1).values 
-    X_pred_studentnumber = dataset_pred[['Studentnummer']]
+    X_pred = pred_df_sdd.drop(dropout_column, axis=1).values 
+    X_pred_studentnumber = dataset_pred[[studentnumber_column]]
     
     yhat2 = best_lasso_model.predict(X_pred)
     pred_data0 = pd.DataFrame({'voorspelling': yhat2})
@@ -36,13 +36,14 @@ def lassoregressionmodel_pred (pred_df_sdd, dataset_pred, random_state = random_
     pred_data['ranking'] = pred_data['voorspelling'].rank(method = 'dense', ascending=False)
     pred_data = pred_data.sort_values(by=['voorspelling'], ascending=False).reset_index(drop=True)
     student_ranked_data = pd.DataFrame(data = pred_data)
-    student_ranked_data_ordered = student_ranked_data[['ranking', 'Studentnummer', 'voorspelling']]
+    student_ranked_data_ordered = student_ranked_data[['ranking', studentnumber_column, 'voorspelling']]
     return student_ranked_data_ordered
 
 # Support Vector Machines for predicting
 def supportvectormachinemodel_pred (pred_df):
-    X_pred = pred_df.drop('Dropout', axis=1).values  
-    X_pred_studentnumber = pred_df[['Studentnummer']]
+    X_pred = pred_df.drop(dropout_column, axis=1).values  
+    X_pred_studentnumber = pred_df[[studentnumber_column]]
+    
     yhat2 = best_svm_model.predict_proba(X_pred)
     yhat2_uitval = yhat2[:, 1]
     pred_data0 = pd.DataFrame({'voorspelling': yhat2_uitval})
@@ -52,5 +53,5 @@ def supportvectormachinemodel_pred (pred_df):
     pred_data['ranking'] = pred_data['voorspelling'].rank(method = 'dense', ascending=False)
     pred_data = pred_data.sort_values(by=['voorspelling'], ascending=False).reset_index(drop=True)
     student_ranked_data = pd.DataFrame(data = pred_data)
-    student_ranked_data_ordered = student_ranked_data[['ranking', 'Studentnummer', 'voorspelling']]
+    student_ranked_data_ordered = student_ranked_data[['ranking', studentnumber_column, 'voorspelling']]
     return student_ranked_data_ordered
--- module/modeling/train.py
+++ module/modeling/train.py
@@ -7,46 +7,51 @@ from module.config import *
 
 # Random Forest Regressor model for training 
 def randomforestregressormodel_train(dataset_train):
-    X = dataset_train.drop("Dropout", axis=1).values
+    X = dataset_train.drop(dropout_column, axis=1).values
     y = dataset_train.Dropout.values
     rf = RandomForestRegressor(random_state=random_seed)
-
-    grid_model = GridSearchCV(rf, rf_parameters, n_jobs = -1, verbose = 2)
+    
+    # Hyperparameter tuning using gridsearchCV
+    grid_model = GridSearchCV(rf, rf_parameters, refit = True, n_jobs = -1, verbose = 2)
     grid_model.fit(X, y) 
     best_params = grid_model.best_params_
     best_rf_model = RandomForestRegressor(**best_params)
     best_rf_model.fit(X, y) 
     
+    # Save model 
     joblib.dump(best_rf_model, 'models/random_forest_regressor.joblib')
     return best_rf_model
 
-
+# lasso regression model for training 
 def lassoregressionmodel_train (dataset_train_sdd): 
-    X = dataset_train_sdd.drop("Dropout", axis=1).values
+    X = dataset_train_sdd.drop(dropout_column, axis=1).values
     y = dataset_train_sdd.Dropout.values
     lasso_model = Lasso(random_state = random_seed)
     param = {'alpha':alpha_range}
-
-    lasso_grid_search = GridSearchCV(lasso_model, param_grid = param, cv=5, n_jobs = -1, verbose = 2)
+    
+    # Hyperparameter tuning using gridsearchCV
+    lasso_grid_search = GridSearchCV(lasso_model, param_grid = param, refit = False, cv=5, n_jobs = -1, verbose = 2)
     lasso_grid_search.fit(X, y)
-
     best_params = lasso_grid_search.best_params_
     best_lasso_model = Lasso(**best_params)
     best_lasso_model.fit(X, y) 
     
+    # Save model 
     joblib.dump(best_lasso_model, 'models/lasso_regression.joblib')
     return best_lasso_model
 
-
+# Support vector machine model for training 
 def supportvectormachinemodel_train(dataset_train):
-    X = dataset_train.drop("Dropout", axis=1).values
+    X = dataset_train.drop(dropout_column, axis=1).values
     y = dataset_train.Dropout.values
     
-    svm_gridsearch = GridSearchCV(SVC(random_state=random_seed, probability = True), svm_parameters, refit = True, n_jobs = -1, verbose = 2) 
+    # Hyperparameter tuning using gridsearchCV    # Hyperparameter tuning using gridsearchCV
+    svm_gridsearch = GridSearchCV(SVC(random_state=random_seed, probability = True), svm_parameters, refit = False, n_jobs = -1, verbose = 2) 
     svm_gridsearch.fit(X, y)
-
     best_params = svm_gridsearch.best_params_
     best_svm_model = SVC(**best_params, probability = True)
     best_svm_model.fit(X, y) 
+
+    # Save model 
     joblib.dump(best_svm_model, 'models/support_vector_machine.joblib')
     return best_svm_model
