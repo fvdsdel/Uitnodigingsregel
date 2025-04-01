55af42f33a2b4c70848b601a31a8fd40b518ab01
MondriaanBI <businessinformation@rocmondriaan.nl>
2025-04-01 16:23:51 +0200

    Updated dataset.py
    Added comments

--- module/dataset.py
+++ module/dataset.py
@@ -7,7 +7,7 @@ def basic_cleaning(dataset):
     dataset_no_dups.loc[:, numerical_cols.columns] = numerical_cols.fillna(numerical_cols.mean())
     return dataset_no_dups
 
-# Find columns in train dataset where all rows contain the same value (excluding the dropout columnn) and then drops them from the train and predict sets
+# Find columns in train dataset where all rows contain the same value and then drop them from the train and predict datasets
 def remove_single_value_columns(dataset_train, dataset_pred):
     single_value_columns = [col for col in dataset_train.columns if dataset_train[col].nunique() == 1]
     dataset_train = dataset_train.drop(columns=single_value_columns, errors='ignore')
