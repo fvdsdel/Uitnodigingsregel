7cdf0648f4386a2c6703ec902971541cde9bef67
fvdsdel <165891554+fvdsdel@users.noreply.github.com>
2025-07-17 16:26:53 +0200

    Update train.py

--- module/modeling/train.py
+++ module/modeling/train.py
@@ -8,58 +8,54 @@ def save_model(model, model_name):
     """Saves the model to a file."""
     joblib.dump(model, f'models/{model_name}.joblib')
 
-# Random Forest Regressor model for training 
-def randomforestregressormodel_train(dataset_train, random_seed, dropout_column, model_params):
-    
+def prep_data(dataset_train,dropout_column):
+    """Prepares the training data by dropping the dropout column."""
     X = dataset_train.drop(dropout_column, axis=1).values
     y = dataset_train[dropout_column].values
+    return X, y
+
+def run_grid_search(estimator, X, y, model_params, grid_kwargs) -> GridSearchCV:
+    """Runs a grid search to find the best hyperparameters for the model."""
+    grid_search = GridSearchCV(estimator, model_params, **grid_kwargs)
+    grid_search.fit(X, y)
+    return grid_search
+
+
+def randomforestregressormodel_train(dataset_train, random_seed, dropout_column, model_params):
+    """Trains a Random Forest Regressor model."""
+    X,y = prep_data(dataset_train, dropout_column)
     estimator_class = RandomForestRegressor
     estimator = estimator_class(random_state=random_seed)
-    
-    # Hyperparameter tuning using gridsearchCV
-    grid_search = GridSearchCV(estimator, model_params, refit = True, n_jobs = -1, verbose = 2)
-    grid_search.fit(X, y) 
-    best_params = grid_search.best_params_
-    best_model = estimator_class(**best_params)
+    grid_kwargs = {'refit':True,'n_jobs': -1, 'verbose': 2}
+    grid_search = run_grid_search(estimator, X, y, model_params, grid_kwargs)
+    best_model = estimator_class(**grid_search.best_params)
     best_model.fit(X, y) 
-    
-    # Save model 
     save_model(best_model, 'random_forest_regressor')
     return best_model
 
-# lasso regression model for training 
+
 def lassoregressionmodel_train (dataset_train_sdd, random_seed, dropout_column, alpha_range):
-    
-    X = dataset_train_sdd.drop(dropout_column, axis=1).values
-    y = dataset_train_sdd[dropout_column].values
+    """Trains a Lasso regression model."""
+    X, y = prep_data(dataset_train_sdd, dropout_column)
     estimator_class = Lasso
     estimator = estimator_class(random_state = random_seed)
-    param = {'alpha':alpha_range}
-    
-    # Hyperparameter tuning using gridsearchCV
-    grid_search = GridSearchCV(estimator, param_grid = param, refit = False, cv=5, n_jobs = -1, verbose = 2)
-    grid_search.fit(X, y)
-    best_params = grid_search.best_params_
-    best_model = estimator_class(**best_params)
+    model_params = {'alpha':alpha_range}
+    grid_kwargs = {'refit': False, 'cv': 5, 'n_jobs': -1, 'verbose': 2}
+    grid_search = run_grid_search(estimator, X, y, model_params, grid_kwargs)
+    best_model = estimator_class(**grid_search.best_params)
     best_model.fit(X, y) 
-    
-    # Save model 
     save_model(best_model, 'lasso_regression')
     return best_model
 
-# Support vector machine model for training 
+
 def supportvectormachinemodel_train(dataset_train_sdd, random_seed, dropout_column, model_params):
-    X = dataset_train_sdd.drop(dropout_column, axis=1).values
-    y = dataset_train_sdd[dropout_column].values
+    """Trains a Support Vector Machine model."""
+    X,y = prep_data(dataset_train_sdd, dropout_column)
     estimator_class = SVC
     estimator = estimator_class(random_state=random_seed, probability = True)
-    # Hyperparameter tuning using gridsearchCV 
-    grid_search = GridSearchCV(estimator, model_params, refit = False, n_jobs = -1, verbose = 2) 
-    grid_search.fit(X, y)
-    best_params = grid_search.best_params_
-    best_model = estimator_class(**best_params, probability = True)
+    grid_kwargs = {'refit': False, 'n_jobs': -1, 'verbose': 2}
+    grid_search = run_grid_search(estimator, X, y, model_params, grid_kwargs)
+    best_model = estimator_class(**grid_search.best_params, probability = True)
     best_model.fit(X, y) 
-
-    # Save model 
     save_model(best_model, 'support_vector_machine')
     return best_model
