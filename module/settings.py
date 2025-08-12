1df08318b28d83a1b5d269669b6a994e67c73358
Frits van der Steege <fritssteege@deltion.nl>
2025-08-12 13:41:11 +0200

    modified:   .gitignore 	modified:   main_direct.ipynb 	modified:   models/lasso_regression.joblib 	modified:   models/random_forest_regressor.joblib 	modified:   models/support_vector_machine.joblib 	modified:   module/settings.py 	modified:   reports/figures/model_usability_dashboard_plot.png 	modified:   reports/model_evaluation.txt 	modified:   reports/model_summary_table.txt 	modified:   reports/threshold_analysis.txt

--- module/settings.py
+++ module/settings.py
@@ -1,3 +1,38 @@
+# import yaml
+
+# class SettingsObject(dict):
+#     # __class__ = dict
+#     def __init__(self, **kwargs):
+#         super().__init__(**kwargs)
+#         for key, value in kwargs.items():
+#             if isinstance(value, dict):
+#                 # Recursively convert nested dictionaries to Struct objects
+#                 setattr(self, key, SettingsObject(**value))
+#             else:
+#                 setattr(self, key, value) # set attribute
+#         # self.__class__ = dict
+#     def __repr__(self):
+#         return str(self.__dict__)
+    
+#     def __str__(self):
+#         return str(self.__dict__)
+    
+#     def as_dict(self):
+#         return self.__dict__
+
+
+# def load_settings(config_file, settings_type = "default") -> dict:
+#     with open(config_file, 'r') as file:
+#         config = yaml.safe_load(file)
+#     if settings_type == 'default':
+#         settings = config['default_settings']
+#     elif settings_type == 'custom':
+#         settings = config['custom_settings']
+#     else:
+#         raise ValueError("No settings found. Choose 'default' or 'custom'.")
+#     return SettingsObject(**settings)
+
+
 import yaml
 
 class SettingsObject(dict):
@@ -19,9 +54,16 @@ class SettingsObject(dict):
     
     def as_dict(self):
         return self.__dict__
+    
+    @property
+    def txt(self):
+        _txt = ""
+        for key, value in self.__dict__.items():
+            _txt = _txt+ f"{key}: {value}\n" 
+        return _txt
 
 
-def load_settings(config_file, settings_type = "default") -> dict:
+def load_settings(config_file, settings_type = "default") -> SettingsObject:
     with open(config_file, 'r') as file:
         config = yaml.safe_load(file)
     if settings_type == 'default':
